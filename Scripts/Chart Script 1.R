da561d632e072280c3e90c5c36e2ad9437d4f9c8
Athiwat <athiwatp@uw.edu>
2021-05-16 02:31:50 +0700

    changes

--- Scripts/Chart Script 1.R
+++ Scripts/Chart Script 1.R
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+vaccinations_df <- read.csv("data/us_state_vaccinations.csv",
+                            stringsAsFactors = FALSE)
+
+>>>>>>> 8702bd8a3503e8780d9316fde1638a29de1e5b9e
 library(dplyr)
 library(ggplot2)
 #Get day of week 
@@ -5,6 +11,7 @@ updated_df <- vaccinations_df %>%
   mutate(day_name = weekdays(as.Date(date)))
 #Condense df
 condensed_df <- updated_df %>%
+<<<<<<< HEAD
   select(new_vaccinations, day_name)
 
 new_condensed_df <- condensed_df %>% 
@@ -27,6 +34,36 @@ bar_plot <- ggplot(data = new_condensed_df) +
                                "",
                                "",
                                "")) +
+=======
+  select(daily_vaccinations, day_name)
+
+#Condense more getting the sum of each day
+new_condensed_df <- condensed_df %>% 
+  group_by(day_name) %>% 
+  summarise(vaccinations = sum(daily_vaccinations, na.rm = T))
+#Sort by weekday
+new_condensed_df$day_name <- factor(new_condensed_df$day_name, 
+                                levels = c("Monday", "Tuesday", "Wednesday",
+                                           "Thursday", "Friday", "Saturday",
+                                           "Sunday"))
+#Bar plot
+bar_plot <- ggplot(data = new_condensed_df) + 
+  geom_col(mapping = aes(x = day_name, y = vaccinations, fill = day_name)) + 
+  scale_fill_manual(values = c("Monday" = "yellow",
+                               "Tuesday" = "orange",
+                               "Wednesday" = "red",
+                               "Thursday" = "blue",
+                               "Friday" = "green",
+                               "Saturday" = "white",
+                               "Sunday" = "grey"), 
+                    labels = c("Monday",
+                               "Tuesday",
+                               "Wednesday",
+                               "Thursday",
+                               "Friday",
+                               "Saturday",
+                               "Sunday")) +
+>>>>>>> 8702bd8a3503e8780d9316fde1638a29de1e5b9e
   ggtitle("Vaccinations by Day of Week") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlab("Day Name") +
@@ -35,6 +72,7 @@ bar_plot <- ggplot(data = new_condensed_df) +
 
 
 
+<<<<<<< HEAD
 bar_plot
 
 #Sort the x-axis by day of week
@@ -42,3 +80,8 @@ condensed_df$day_name <- factor(updated_df$day_name,
                                 levels = c("Monday", "Tuesday", "Wednesday",
                                            "Thursday", "Friday", "Saturday",
                                            "Sunday"))
+=======
+
+
+bar_plot
+>>>>>>> 8702bd8a3503e8780d9316fde1638a29de1e5b9e
