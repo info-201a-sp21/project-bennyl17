96edf2ee03933ce774ba0659bd0cc282ae636188
Athiwat <athiwatp@uw.edu>
2021-05-20 15:26:42 +0700

    fixed script 3 to funciton type

--- Scripts/Chart Script 3.R
+++ Scripts/Chart Script 3.R
@@ -1,39 +1,29 @@
-
-df <- read.csv("data/WA_COVID19_Cases.csv",
-               stringsAsFactors = FALSE)
-library(dplyr)
-library(ggplot2)
-#variables
-age0_19 <- sum(df$Age.0.19, na.rm = T)
-age20_34 <- sum(df$Age.20.34, na.rm = T)
-age35_49 <- sum(df$Age.35.49, na.rm = T)
-age50_64 <- sum(df$Age.50.64, na.rm = T)
-age65_79 <- sum(df$Age.65.79, na.rm = T)
-age80_up <- sum(df$Age.80., na.rm = T)
-#new dataframe
-age <- c("Ages 0-19", "Ages 20-34", "Ages 35-49", "Ages 50-64",
-         "Ages 65-79", "Ages 80+")
-sum_of_cases <- c(age0_19, age20_34, age35_49, age50_64, age65_79, age80_up)
-new_df <- data.frame(sum_of_cases, age)
-
-
-
-#pie chart
-new_df <- new_df %>%
-  arrange(desc(age)) %>%
-  mutate(prop = sum_of_cases / sum(new_df$sum_of_cases) * 100) %>%
-  mutate(ypos = cumsum(prop) - 0.5 * prop)
-
-
-
-pie_chart <- ggplot(new_df, aes(x = "", y = prop, fill = age)) +
-  geom_bar(stat = "identity", width = 1, color = "black") +
-  coord_polar("y", start = 0) +
-  ggtitle("Cases by Age group in Washington") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme_void() +
-  geom_text(aes(y = ypos, label = sum_of_cases), color = "black", size = 3) +
-  coord_polar("y", start = 180)
-
-
+# Need comments
+pie_chart_df <- function(df) {
+  updated_df <- df %>% 
+    summarise(age0_19 = sum(df$Age.0.19, na.rm = T),
+              age20_34 = sum(df$Age.20.34, na.rm = T),
+              age35_49 = sum(df$Age.35.49, na.rm = T),
+              age50_64 = sum(df$Age.50.64, na.rm = T),
+              age65_79 = sum(df$Age.65.79, na.rm = T),
+              age80_up = sum(df$Age.80., na.rm = T)) %>% 
+    gather("Age", "sum_of_cases") %>% 
+    arrange(desc(Age)) %>%
+    mutate(prop = sum_of_cases / sum(sum_of_cases) * 100) %>%
+    mutate(ypos = cumsum(prop) - 0.5 * prop)
+  
+  ggplot(data = updated_df, aes(x = factor(1), y = prop, fill = Age)) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar("y", start = 180) +
+    ggtitle("Cases by Age group in Washington") +
+    theme_void() +
+    geom_text(aes(y = ypos, label = sum_of_cases), color = "black", size = 3) +
+    scale_fill_discrete(name = "Ages", 
+                        labels = c("Age 0 to 19", 
+                                   "Age 20 to 34",
+                                   "Age 35 to 49",
+                                   "Age 50 to 64",
+                                   "Age 65 to 79",
+                                   "Age 80+"))
+}
 
