48457497d79dbd03903be9328396ad4dfb38968c
Athiwat <athiwatp@uw.edu>
2021-05-29 16:57:11 +0700

    add somewhat a skeleton for the application

---
+++ app_ui.R
@@ -0,0 +1,23 @@
+ui <- navbarPage(
+  "Covid Project",
+  introduction_page,
+  interactive_page_one,
+  interactive_page_two,
+  interactive_page_three,
+  summary_page
+)
+
+home_page <- tabPanel(
+)
+
+interactive_page_one <- tabPanel(
+)
+
+interactive_page_two <- tabPanel(
+)
+
+interactive_page_three <- tabPanel(
+)
+
+summary_page <- tabPanel(
+)
