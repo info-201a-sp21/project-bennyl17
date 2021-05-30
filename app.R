735148aa12c6e7e61fa8995c3d4bce893af21b9c
Athiwat <athiwatp@uw.edu>
2021-05-31 00:13:33 +0700

    added some more comments

--- app.R
+++ app.R
@@ -1,9 +1,13 @@
+# Remove global elements for easier debugging. 
 rm(list = ls())
 
+# Load all the necessary libraries
 library(shiny)
 library(dplyr)
 
+# Source the UI and Server files
 source("app_ui.R")
 source("app_server.R")
 
+# Create the shiny application
 shinyApp(ui = ui, server = server)
--- app_server.R
+++ app_server.R
@@ -1,2 +1,11 @@
+# Sources of scripts where we will call functions
+# example: source("./scripts/...R")
+
+# Start of shiny server function
 server <- function(input, output) {
+  
+  # Earth: I'm going to create a map
+  output$map <- renderPlotly({ 
+  })
+  
 }
