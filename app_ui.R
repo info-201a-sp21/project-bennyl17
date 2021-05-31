64a8381aa10cc9eb6f5f1819d5285aae179c0f33
Athiwat <athiwatp@uw.edu>
2021-05-31 08:29:43 +0700

    fix to include css styles

--- app_ui.R
+++ app_ui.R
@@ -1,10 +1,13 @@
-ui <- navbarPage(
-  "Covid Project",
-  introduction_page,
-  interactive_page_one,
-  interactive_page_two,
-  interactive_page_three,
-  summary_page
+ui <- fluidpage(
+  includeCSS("style.css"),
+  navbarPage(
+    "Covid Project",
+    introduction_page,
+    interactive_page_one,
+    interactive_page_two,
+    interactive_page_three,
+    summary_page
+  )
 )
 
 home_page <- tabPanel(
@@ -14,7 +17,8 @@ home_page <- tabPanel(
 )
 
 interactive_page_one <- tabPanel(
-  
+  "Covid-19 Map",
+  h1("")
 )
 
 interactive_page_two <- tabPanel(
